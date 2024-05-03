7973502a5558dc42863742beb89e5d27cde84c57
AT5HK <at5hkx0x0@gmail.com>
2024-05-03 00:05:49 -0700

    first test commit

--- TestApp123/ViewController.swift
+++ TestApp123/ViewController.swift
@@ -12,7 +12,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = .green
+        let iOSVersion = getiOSVersion()
+        let sdkVersion = getSDKName()
+        print("iOS Version: \(iOSVersion)")
+        print("iOS SDK Version: \(sdkVersion)")
+        
     }
+    
+    func getiOSVersion() -> String {
+        return UIDevice.current.systemVersion
+    }
+    
+    func getSDKName() -> String {
+        if let SDKName = Bundle.main.infoDictionary?["DTSDKName"] {
+            return SDKName as! String
+        }
+        
+        return "SDK name not found"
+    }
+
+    
 
 
 }
